7b8cc5eba6f4922128e95e4441e91c196a9d8cfa
Tim <tim.daubner@outlook.com>
2025-07-23 18:10:17 +0200

    commit feedback work

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+function sendMail(event) {
+    event.preventDefault();
+    const data = new FormData(event.target);
+
+    fetch("https://formspree.io/f/mblkgzry", {
+        method: "POST",
+        body: new FormData(event.target),
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(() => {
+        window.location.href = "./contact_send.html";
+    }).catch((error) => {
+        console.log(error);
+    });
+}
+
 function toggleMenu() {
     document.getElementById('burger-menu').classList.toggle('d_none');
 }
